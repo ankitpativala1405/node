af01f0d2a7842d0cf2e3ede7b25327881e0c3022
Your Name <you@example.com>
2025-04-19 21:16:09 +0530

    update

---
+++ index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const app=express()
+
+app.use(express.json());
+
+ let mongo = `mongodb+srv://nodepractice:<db_password>@cluster0.sk2g4jt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+app.get("/test",(request,responce)=>{
+    responce.send("ankit")
+})
+
+const mongoose = require("mongoose");
+
+const dbconnect = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://nodepractice:Ankit@cluster0.sk2g4jt.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("Mongoose connected ..... ");
+  } catch (error) {
+    console.error("Connection error:", error);
+  }
+};
+
+dbconnect();
+
+
+app.listen(8472,()=>{
+    console.error("server start with 8472");
+    
+})
+
