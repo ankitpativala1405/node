9cce78b30b188e881551ec360b4936690c85030b
Your Name <you@example.com>
2025-04-19 08:35:17 +0530

    update

--- express/index.js
+++ express/index.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const app=express()
+
+app.get("/test",(request,responce)=>{
+    responce.send("ankit")
+})
+
+
 app.listen(8472,()=>{
     console.log("server start with 8472");
     
-})
-app.get("/test",(request,responce)=>{
-    responce.send("ankit")
 })
