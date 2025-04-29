a18592c4eebfb9f0f84c3c399020c3e99fcf8acc
Your Name <you@example.com>
2025-04-29 19:12:48 +0530

    update

--- project/index.js
+++ project/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const DBConnect = require("./config/dbconnect");
 const app = express();
+const cors = require("cors");
+const router = require("./routers/user");
+app.use(cors());
 require("dotenv").config();
 
 app.use(express.json());
-const PORT = process.env.PORT || 8472;
+const PORT = process.env.PORT || 4000;
+
+
+
+app.use("/api/users",router);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
   DBConnect();
 });
+
--- project/models/schema.js
+++ project/models/schema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose(
+const UserSchema = new mongoose.Schema(
   {
     name: { type: String },
     email: { type: String, required: true },
